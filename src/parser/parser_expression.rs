45e4826eaf0f9f4aae7326a29e39d384ac671fa9
JonathanBrouwer <jonathantbrouwer@gmail.com>
2021-05-21 00:24:47 +0200

    Add expression control

--- src/parser/parser_expression.rs
+++ src/parser/parser_expression.rs
@@ -6,6 +6,7 @@ use crate::lexer::lexer::LexerToken::*;
 #[derive(Debug, Eq, PartialEq)]
 pub enum Expression<'a> {
     Name(&'a str),
+    Control(&'a str),
     ExpressionSequence(Vec<Expression<'a>>),
     Block(Vec<Expression<'a>>),
 }
@@ -14,13 +15,13 @@ impl<'a> Expression<'a> {
     pub fn print_indented(&self, indent: usize) {
         match self {
             Expression::Name(n) => print!("{}", n),
+            Expression::Control(n) => print!("{}", n),
             Expression::ExpressionSequence(vec) => for exp in vec {
                 exp.print_indented(indent);
                 print!(" ");
             }
             Expression::Block(vec) => {
-
-                for (n, exp) in vec.iter().enumerate() {
+                for exp in vec {
                     println!();
                     let indent = indent + 2;
                     print!("{:indent$}", "", indent=indent);
@@ -34,15 +35,17 @@ impl<'a> Expression<'a> {
 pub fn parse_expression<'a>(input: &'a [LexerItem<'a>]) -> Result<(Expression, &'a [LexerItem<'a>]), ParseError> {
     let (mut res, mut input) = one_or_more(input, |input|
         alt(input, vec![
-            |input| parse_expression_parens(input),
             |input| parse_expression_name(input),
+            |input| parse_expression_control(input),
         ])
     )?;
+
     //Block can only occur at the end of the sequence
     if let Ok((nr, ni)) = parse_expression_block(input) {
         res.push(nr);
         input = ni;
     }
+
     if res.len() == 1 {
         Ok((res.into_iter().next().unwrap(), input))
     } else {
@@ -50,16 +53,14 @@ pub fn parse_expression<'a>(input: &'a [LexerItem<'a>]) -> Result<(Expression, &
     }
 }
 
-pub fn parse_expression_parens<'a>(input: &'a [LexerItem<'a>]) -> Result<(Expression, &'a [LexerItem<'a>]), ParseError> {
-    let input = expect_control_keyword(input, "(")?;
-    let (expr, input) = parse_expression(input)?;
-    let input = expect_control_keyword(input, ")")?;
-    Ok((expr, input))
-}
-
 pub fn parse_expression_name<'a>(input: &'a [LexerItem<'a>]) -> Result<(Expression, &'a [LexerItem<'a>]), ParseError> {
     let (name, input) = expect_name(input)?;
     Ok((Expression::Name(name), input))
+}
+
+pub fn parse_expression_control<'a>(input: &'a [LexerItem<'a>]) -> Result<(Expression, &'a [LexerItem<'a>]), ParseError> {
+    let (name, input) = expect_control(input)?;
+    Ok((Expression::Control(name), input))
 }
 
 pub fn parse_expression_block<'a>(input: &'a [LexerItem<'a>]) -> Result<(Expression, &'a [LexerItem<'a>]), ParseError> {
