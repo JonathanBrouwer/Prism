4b4c0573ba839ae80915ceaa1d0c4a4ac0f9863b
JonathanBrouwer <jonathantbrouwer@gmail.com>
2021-05-27 12:59:02 +0200

    Left recursion sorta working

--- src/parser/peg_parser.rs
+++ src/parser/peg_parser.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use std::hash::Hash;
 
 use crate::parser::customizable_parser::ParseRule::Expect;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque, HashSet};
 
 pub trait Token<TT: TokenType>: Sized + Copy + Eq + Hash {
     fn to_type(&self) -> TT;
@@ -75,159 +75,222 @@ pub fn combine_err<'a, TT: TokenType, T: Token<TT>>(a: Option<ParseError<'a, TT,
     }
 }
 
-type ParseResult<'a, TT, T> = Result<ParseSuccess<'a, TT, T>, ParseError<'a, TT, T>>;
-
-pub struct ParserCache<'a, TT: TokenType, T: Token<TT>> {
-    cache: HashMap<(usize, usize) , ParseResult<'a, TT, T>>
+struct ParserCache<'a, TT: TokenType, T: Token<TT>> {
+    cache: HashMap<(usize, usize) , ParseResult<'a, TT, T>>,
+    cache_layers: VecDeque<HashSet<(usize, usize)>>
 }
 
 impl<'a, TT: TokenType, T: Token<TT>> ParserCache<'a, TT, T> {
-    pub fn new() -> ParserCache<'a, TT, T> {
-        ParserCache{ cache: HashMap::with_capacity(256) }
+    pub fn get(&mut self, key: &(usize, usize)) -> Option<&ParseResult<'a, TT, T>> {
+        self.cache.get(key)
     }
-    pub fn with_capacity(len: usize) -> ParserCache<'a, TT, T> {
-        ParserCache{ cache: HashMap::with_capacity(64 * len) }
+
+    pub fn remove(&mut self, key: &(usize, usize)) -> Option<ParseResult<'a, TT, T>> {
+        self.cache.remove(key)
+    }
+
+    pub fn contains_key(&mut self, key: &(usize, usize)) -> bool {
+        self.cache.contains_key(key)
+    }
+
+    pub fn insert(&mut self, key: (usize, usize), v: ParseResult<'a, TT, T>) {
+        self.cache.insert(key, v);
+        self.cache_layers.back_mut().unwrap().insert(key);
+    }
+
+    pub fn layer_incr(&mut self) {
+        self.cache_layers.push_back(HashSet::new());
+    }
+
+    pub fn layer_decr(&mut self) {
+        for key in self.cache_layers.pop_back().unwrap() {
+            self.cache.remove(&key);
+        }
     }
 }
+
+type ParseResult<'a, TT, T> = Result<ParseSuccess<'a, TT, T>, ParseError<'a, TT, T>>;
 
 pub fn next<'a, TT: TokenType, T: Token<TT>>(input: &'a [T]) -> (&'a T, &'a [T]) {
     assert!(input.len() > 0);
     (&input[0], &input[1..])
 }
 
-pub fn parse<'a, TT: TokenType, T: Token<TT>>(input: &'a [T], rule: usize, glb_rules: &Vec<PegRule<TT, T>>, cache: &mut ParserCache<'a, TT, T>) -> ParseResult<'a, TT, T> {
-    //Check if result is in cache
-    let key = (input.len(), rule);
-    if cache.cache.contains_key(&key) {
-        return cache.cache.get(&key).unwrap().clone();
+pub struct Parser<'a, TT: TokenType, T: Token<TT>> {
+    glb_rules: &'a Vec<PegRule<TT, T>>,
+
+    cache: ParserCache<'a, TT, T>,
+
+    can_be_empty: Vec<bool>
+}
+
+impl<'a, TT: TokenType, T: Token<TT>> Parser<'a, TT, T> {
+    pub fn new(rules: &'a Vec<PegRule<TT, T>>) -> Self {
+        let mut s = Parser {
+            glb_rules: rules,
+            cache: ParserCache { cache: HashMap::new(), cache_layers: VecDeque::new() },
+            can_be_empty: Vec::with_capacity(rules.len())
+        };
+        s.cache.layer_incr();
+        s
     }
 
-    //Deal with left recursion
-    let mut prev_best = usize::MAX;
-
-    //TODO this will be thrown for purely recursive rules
-    cache.cache.insert(key, Err(ParseError { on: &input[0], expect: vec![], inv_priority: 0 }));
-    let res = parse_sub(input, rule, glb_rules, cache);
-    cache.cache.insert(key, res.clone());
-    res
-
-    // loop {
-    //     let res = parse_sub(input, rule, glb_rules, cache);
-    //
-    //     match res {
-    //         Ok(v) => {
-    //             //Did we do better?
-    //             if v.rest.len() < prev_best {
-    //                 prev_best = v.rest.len();
-    //                 cache.cache.insert(key, Ok(v));
-    //             } else {
-    //                 assert_eq!(v.rest.len(), prev_best);
-    //                 return Ok(v);
-    //             }
-    //         },
-    //         Err(v) => {
-    //             return Err(v)
-    //         }
+    // fn can_be_empty(&mut self, rule: usize) -> bool {
+    //     let rule = &self.glb_rules[rule];
+    //     let res = match rule {
+    //         PegRule::LiteralExact(_) => {}
+    //         PegRule::LiteralBind(_) => {}
+    //         PegRule::Sequence(_) => {}
+    //         PegRule::ChooseFirst(_) => {}
+    //         PegRule::Repeat(_, _, _) => {}
+    //         PegRule::Option(_) => {}
+    //         PegRule::LookaheadPositive(_) => {}
+    //         PegRule::LookaheadNegative(_) => {}
     //     }
+    //     self.can_be_empty[]
     // }
-}
 
-pub fn parse_sub<'a, TT: TokenType, T: Token<TT>>(input: &'a [T], rule: usize, glb_rules: &Vec<PegRule<TT, T>>, cache: &mut ParserCache<'a, TT, T>) -> ParseResult<'a, TT, T> {
-    let rule = &glb_rules[rule];
-    match rule {
-        PegRule::LiteralExact(expect) => {
-            let (token, rest) = next(input);
-            if *token == *expect {
-                Ok(ParseSuccess { result: (), best_error: None, rest })
-            } else {
-                Err(ParseError { on: &input[0], expect: vec![Expected::LiteralExact(*expect)], inv_priority: input.len()})
-            }
+    pub fn parse(&mut self, input: &'a [T], rule: usize) -> ParseResult<'a, TT, T> {
+        //Check if result is in cache
+        let key = (input.len(), rule);
+        if self.cache.contains_key(&key) {
+            return self.cache.get(&key).unwrap().clone();
         }
-        PegRule::LiteralBind(expect) => {
-            let (token, rest) = next(input);
-            if token.to_type() == *expect {
-                Ok(ParseSuccess { result: (), best_error: None, rest })
-            } else {
-                Err(ParseError { on: &input[0], expect: vec![Expected::LiteralBind(*expect)], inv_priority: input.len() })
-            }
-        }
-        PegRule::Sequence(rules) => {
-            let mut rest = input;
-            let mut best_error: Option<ParseError<'a, TT, T>> = None;
-            for &rule in rules {
-                match parse(rest, rule, glb_rules, cache) {
-                    Ok(suc) => {
-                        rest = suc.rest;
-                        best_error = combine_err(best_error, suc.best_error);
+
+        //Deal with left recursion
+        let mut prev_best = usize::MAX;
+
+        //TODO this will be thrown for purely recursive rules
+        self.cache.layer_incr();
+        self.cache.insert(key, Err(ParseError { on: &input[0], expect: vec![], inv_priority: 0 }));
+
+        loop {
+            let res = self.parse_sub(input, rule);
+
+            match res {
+                Ok(v) => {
+                    //Did we do better?
+                    if v.rest.len() < prev_best {
+                        prev_best = v.rest.len();
+                        self.cache.layer_decr();
+                        self.cache.layer_incr();
+                        self.cache.insert(key, Ok(v));
+                    } else {
+                        let prev_v = self.cache.remove(&key).unwrap();
+                        self.cache.layer_decr();
+                        self.cache.insert(key, prev_v.clone());
+                        return prev_v;
                     }
-                    Err(fail) => {
-                        best_error = combine_err(best_error, Some(fail));
-                        return Err(best_error.unwrap());
-                    }
+                },
+                Err(v) => {
+                    //TODO what if next call errors
+                    self.cache.layer_decr();
+                    self.cache.insert(key, Err(v.clone()));
+                    return Err(v)
                 }
             }
-            Ok(ParseSuccess { result: (), best_error, rest })
         }
-        PegRule::ChooseFirst(rules) => {
-            assert!(rules.len() > 0);
-            let mut best_error: Option<ParseError<'a, TT, T>> = None;
-            for &rule in rules {
-                match parse(input, rule, glb_rules, cache) {
-                    Ok(suc) => {
-                        best_error = combine_err(best_error, suc.best_error);
-                        return Ok(ParseSuccess { result: suc.result, rest: suc.rest, best_error });
-                    }
-                    Err(fail) => {
-                        best_error = combine_err(best_error, Some(fail));
-                    }
+    }
+
+    fn parse_sub(&mut self, input: &'a [T], rule: usize) -> ParseResult<'a, TT, T> {
+        let rule = &self.glb_rules[rule];
+        match rule {
+            PegRule::LiteralExact(expect) => {
+                let (token, rest) = next(input);
+                if *token == *expect {
+                    Ok(ParseSuccess { result: (), best_error: None, rest })
+                } else {
+                    Err(ParseError { on: &input[0], expect: vec![Expected::LiteralExact(*expect)], inv_priority: input.len()})
                 }
             }
-            Err(best_error.unwrap())
-        }
-        PegRule::Repeat(rule, min, max) => {
-            let mut rest = input;
-            let mut best_error: Option<ParseError<'a, TT, T>> = None;
+            PegRule::LiteralBind(expect) => {
+                let (token, rest) = next(input);
+                if token.to_type() == *expect {
+                    Ok(ParseSuccess { result: (), best_error: None, rest })
+                } else {
+                    Err(ParseError { on: &input[0], expect: vec![Expected::LiteralBind(*expect)], inv_priority: input.len() })
+                }
+            }
+            PegRule::Sequence(rules) => {
+                let mut rest = input;
+                let mut best_error: Option<ParseError<'a, TT, T>> = None;
+                for &rule in rules {
+                    match self.parse(rest, rule) {
+                        Ok(suc) => {
+                            rest = suc.rest;
+                            best_error = combine_err(best_error, suc.best_error);
+                        }
+                        Err(fail) => {
+                            best_error = combine_err(best_error, Some(fail));
+                            return Err(best_error.unwrap());
+                        }
+                    }
+                }
+                Ok(ParseSuccess { result: (), best_error, rest })
+            }
+            PegRule::ChooseFirst(rules) => {
+                assert!(rules.len() > 0);
+                let mut best_error: Option<ParseError<'a, TT, T>> = None;
+                for &rule in rules {
+                    match self.parse(input, rule) {
+                        Ok(suc) => {
+                            best_error = combine_err(best_error, suc.best_error);
+                            return Ok(ParseSuccess { result: suc.result, rest: suc.rest, best_error });
+                        }
+                        Err(fail) => {
+                            best_error = combine_err(best_error, Some(fail));
+                        }
+                    }
+                }
+                Err(best_error.unwrap())
+            }
+            PegRule::Repeat(rule, min, max) => {
+                let mut rest = input;
+                let mut best_error: Option<ParseError<'a, TT, T>> = None;
 
-            //Do minimum amount of times
-            for _ in 0..(min.unwrap_or(0)) {
-                let res = parse(input, *rule, glb_rules, cache)?;
-                rest = res.rest;
-                best_error = combine_err(best_error, res.best_error);
-            }
+                //Do minimum amount of times
+                for _ in 0..(min.unwrap_or(0)) {
+                    let res = self.parse(input, *rule)?;
+                    rest = res.rest;
+                    best_error = combine_err(best_error, res.best_error);
+                }
 
-            //Do from minimum to maximum amount of times
-            for _ in (min.unwrap_or(0))..(max.unwrap_or(usize::MAX)) {
-                let res = match parse(rest, *rule, glb_rules, cache) {
-                    Ok(v) => v,
-                    Err(_) => return Ok(ParseSuccess { result: (), rest, best_error})
-                };
-                rest = res.rest;
-                best_error = combine_err(best_error, res.best_error);
-            }
+                //Do from minimum to maximum amount of times
+                for _ in (min.unwrap_or(0))..(max.unwrap_or(usize::MAX)) {
+                    let res = match self.parse(rest, *rule) {
+                        Ok(v) => v,
+                        Err(_) => return Ok(ParseSuccess { result: (), rest, best_error})
+                    };
+                    rest = res.rest;
+                    best_error = combine_err(best_error, res.best_error);
+                }
 
-            return Ok(ParseSuccess { result: (), rest, best_error})
-        }
-        PegRule::Option(rule) => {
-            match parse(input, *rule, glb_rules, cache) {
-                Ok(v) => Ok(v),
-                Err(_) => return Ok(ParseSuccess { result: (), rest: input, best_error: None})
+                return Ok(ParseSuccess { result: (), rest, best_error})
             }
-        }
-        PegRule::LookaheadPositive(rule) => {
-            match parse(input, *rule, glb_rules, cache) {
-                //TODO should we return best error?
-                Ok(v) => Ok(ParseSuccess {result: v.result, best_error: None, rest: input}),
-                Err(e) => return Err(e)
+            PegRule::Option(rule) => {
+                match self.parse(input, *rule) {
+                    Ok(v) => Ok(v),
+                    Err(_) => return Ok(ParseSuccess { result: (), rest: input, best_error: None})
+                }
             }
-        }
-        PegRule::LookaheadNegative(rule) => {
-            match parse(input, *rule, glb_rules, cache) {
-                Ok(_) => Err(ParseError{ on: &input[0], expect: vec![], inv_priority: input.len()}),
-                Err(_) => Ok(ParseSuccess {result: (), best_error: None, rest: input})
+            PegRule::LookaheadPositive(rule) => {
+                match self.parse(input, *rule) {
+                    //TODO should we return best error?
+                    Ok(v) => Ok(ParseSuccess {result: v.result, best_error: None, rest: input}),
+                    Err(e) => return Err(e)
+                }
+            }
+            PegRule::LookaheadNegative(rule) => {
+                match self.parse(input, *rule) {
+                    Ok(_) => Err(ParseError{ on: &input[0], expect: vec![], inv_priority: input.len()}),
+                    Err(_) => Ok(ParseSuccess {result: (), best_error: None, rest: input})
+                }
             }
         }
     }
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -265,7 +328,8 @@ mod tests {
         let rules = vec![
             PegRule::LiteralExact(T::A)
         ];
-        assert!(parse(input, 0, &rules, &mut ParserCache::new()).is_ok())
+        let mut parser = Parser::new(&rules);
+        assert!(parser.parse(input, 0).is_ok());
     }
 
     #[test]
@@ -274,7 +338,8 @@ mod tests {
         let rules = vec![
             PegRule::LiteralExact(T::B)
         ];
-        assert!(parse(input, 0, &rules, &mut ParserCache::new()).is_err())
+        let mut parser = Parser::new(&rules);
+        assert!(parser.parse(input, 0).is_err());
     }
 
     #[test]
@@ -282,14 +347,14 @@ mod tests {
         let input = &[T::A, T::A, T::A, T::C];
         let rules = vec![
             PegRule::Sequence(vec![1, 4]), // S = XC
-            PegRule::ChooseFirst(vec![2, 3]), // X = Y | e
+            PegRule::ChooseFirst(vec![2, 5]), // X = Y | e
             PegRule::Sequence(vec![1, 3]), // Y = XA
             PegRule::LiteralExact(T::A),
             PegRule::LiteralExact(T::C),
             PegRule::Sequence(vec![])
         ];
-        let res = parse(input, 0, &rules, &mut ParserCache::new());
-        assert!(res.is_ok())
+        let mut parser = Parser::new(&rules);
+        assert!(parser.parse(input, 0).is_ok());
     }
 
     #[test]
@@ -303,6 +368,7 @@ mod tests {
             PegRule::LiteralExact(T::C),
             PegRule::Sequence(vec![])
         ];
-        assert!(parse(input, 0, &rules, &mut ParserCache::new()).is_ok())
+        let mut parser = Parser::new(&rules);
+        assert!(parser.parse(input, 0).is_ok());
     }
 }
