eefbc1cb735fa780756e72e34f18c281741f62ee
jonathan <jonathantbrouwer@gmail.com>
2022-12-11 19:01:09 +0100

    Fix jonla compiler

--- jonla-compiler/src/main.rs
+++ jonla-compiler/src/main.rs
@@ -1,33 +1,28 @@
+use jonla_parser::grammar::GrammarFile;
+use jonla_parser::parse_grammar;
+use jonla_parser::parser::actual::error_printer::*;
+use jonla_parser::parser::actual::parser_rule::run_parser_rule;
+use jonla_parser::parser::core::stream::StringStream;
+
 fn main() {
-    // let grammar: GrammarFile =
-    //     match grammar::grammar_def::toplevel(include_str!("../../jonla-parser-bootstrap/resources/meta.grammar")) {
-    //         Ok(ok) => ok,
-    //         Err(err) => {
-    //             panic!("{}", err);
-    //         }
-    //     };
+    let grammar = include_str!("../resources/grammar");
+    let grammar: GrammarFile = match parse_grammar(grammar) {
+        Ok(ok) => ok,
+        Err(e) => {
+            print_set_error(e, "grammar", grammar, false);
+            return;
+        },
+    };
 
-    // let input = include_str!("../../jonla-parser-bootstrap/resources/meta.grammar");
-    // let input_stream: StringStream = input.into();
-    // let result: Result<_, _> = run_parser_rule(&grammar, "toplevel", input_stream);
-    //
-    // match result {
-    //     Ok(o) => {
-    //         let grammar2 = parse_grammarfile(&*o.1, input);
-    //         assert_eq!(grammar, grammar2);
-    //         println!("{:?}", grammar2)
-    //     }
-    //     Err(e) => print_tree_error(e, "file", input, true),
-    // }
 
-    // let filename = "program.jnl";
-    // let input = include_str!("../resources/program.jnl");
-    // let input_stream: StringStream = input.into();
-    // let result: Result<_, _> = run_parser_rule(&grammar, "block", input_stream);
-    //
-    // match result {
-    //     Ok(o) => println!("Result: {:?}", o.1.to_string(input)),
-    //     // Err(e) => print_set_error(e, filename, input, false),
-    //     Err(e) => print_tree_error(e, filename, input, true),
-    // }
+
+    let filename = "program.jnl";
+    let input = include_str!("../resources/program.jnl");
+    let result: Result<_, _> = run_parser_rule(&grammar, "block", StringStream::new(input));
+
+    match result {
+        Ok(o) => println!("Result: {:?}", o.1.to_string(input)),
+        // Err(e) => print_set_error(e, filename, input, false),
+        Err(e) => print_tree_error(e, filename, input, true),
+    }
 }
