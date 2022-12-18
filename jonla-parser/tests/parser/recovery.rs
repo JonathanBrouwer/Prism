3acd7b57a4b89de987365d0d0955b1d11322e8fe
jonathan <jonathantbrouwer@gmail.com>
2022-12-18 16:44:52 +0100

    Error recovery tests

--- jonla-parser/tests/parser/recovery.rs
+++ jonla-parser/tests/parser/recovery.rs
@@ -1,44 +1,45 @@
 use crate::parser::parse_test;
-// parse_test! {
-// name: recovery1
-// syntax: r#"
-// rule start = "abcd"
-// "#
-// passing tests:
-//     "abcd" => "'abcd'"
-//
-// failing tests:
-//     "" => "0..0"
-//     "a" => "1..1"
-//     "b" => "0..0 1..1"
-//     "abd" => "2..2"
-//     "abc" => "3..3"
-//     "ad" => "1..1"
-//     "ac" => "1..1 2..2"
-//     "axcd" => "1..2"
-//     "axyd" => "1..4"
-//     "xabcd" => "0..2"
-// }
-//
-// parse_test! {
-// name: recovery_with_norecovery
-// syntax: r#"
-// rule start = test*
-//
-// rule test:
-//     w <- w:@str(word) ";"
-//
-// rule word:
-//     @disable_recovery
-//     ['a'-'z']+
-// "#
-// passing tests:
-//     "aaaaa;a;aa;" => "['aaaaa', 'a', 'aa']"
-//
-// failing tests:
-//     "a@a;aa;" => "1..2"
-//     "a@;aa;" => "1..3"
-// }
+
+parse_test! {
+name: recovery1
+syntax: r#"
+rule start = "a" "b" "c" "d"
+"#
+passing tests:
+    "abcd" => "ERROR[sequence]"
+
+failing tests:
+    "" => "0..0"
+    "a" => "1..1"
+    "b" => "0..0 1..1"
+    "abd" => "2..2"
+    "abc" => "3..3"
+    "ad" => "1..1"
+    "ac" => "1..1 2..2"
+    "axcd" => "1..2"
+    "axyd" => "1..3"
+    "xabcd" => "0..2"
+}
+
+parse_test! {
+name: recovery_with_norecovery
+syntax: r#"
+rule start = test*
+
+rule test:
+    w <- w:@str(word) ";"
+
+rule word:
+    @disable_recovery
+    ['a'-'z']+
+"#
+passing tests:
+    "aaaaa;a;aa;" => "['aaaaa', 'a', 'aa']"
+
+failing tests:
+    "a@a;aa;" => "1..2"
+    "a@;aa;" => "1..2"
+}
 
 
 parse_test! {
@@ -54,6 +55,8 @@ passing tests:
     "{abc;}{abc;}{abc;}" => "[ERROR[sequence], ERROR[sequence], ERROR[sequence]]"
 
 failing tests:
-    "{abc}{abc;}{abc;}" => "4..20"
-    "{abx;}{abc;}{abc;}" => "1..3"
+    "{abc}{abc;}{abc;}" => "4..4"
+    "{abx;}{abc;}{abc;}" => "3..4"
+    "{ab;}{abc;}{ac;}" => "3..3 13..14"
+    "{abc}{abc;}{abc;}" => "4..4"
 }
