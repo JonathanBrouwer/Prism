61b68f8c329f8814e148616457e5404dc17e8e81
jonathan <jonathantbrouwer@gmail.com>
2023-08-21 15:30:37 +0200

    Add a few more tests

--- jonla-parser/tests/parser/parametric.rs
+++ jonla-parser/tests/parser/parametric.rs
@@ -34,3 +34,50 @@ passing tests:
 failing tests:
     "x"
 }
+
+parse_test! {
+name: parametric_first_order_multiple
+syntax: r##"
+    rule start:
+        "x" <- a_then_b(a, b)
+
+    rule a():
+        "a"
+
+    rule b():
+        "b"
+
+    rule a_then_b(a, b):
+        a b
+
+    rule layout = " "
+    "##
+passing tests:
+    "ab" => "'x'"
+    "a b" => "'x'"
+failing tests:
+    "a"
+    "b"
+}
+
+parse_test! {
+name: pass_through
+syntax: r##"
+    rule start:
+        "x" <- a(hash)
+
+    rule a(r):
+        b(r)
+
+    rule b(r):
+        r
+
+    rule hash = "#"
+
+    rule layout = " "
+    "##
+passing tests:
+    "#" => "'x'"
+failing tests:
+    "a"
+}
