9d8a8453e56d33abe81f8636bb0326e2b20ed5f3
Jonathan Brouwer <jonathan.brouwer@technolution.nl>
2024-07-03 18:00:25 +0200

    Small fix for variable binding; more failing tests

--- prism-compiler/src/lang/from_parse_env.rs
+++ prism-compiler/src/lang/from_parse_env.rs
@@ -1,20 +1,33 @@
+use rpds::{List, RedBlackTreeMap};
 use crate::desugar::{Guid, ParseEnv, ParseIndex, SourceExpr};
 use crate::lang::error::TypeError;
 use crate::lang::{PartialExpr, TcEnv, UnionIndex, ValueOrigin};
-use crate::lang::env::GenericEnv;
 
 /// Stores the variables in scope + the depth of the scope
-#[derive(Default, Clone)]
+#[derive(Clone)]
 struct Scope<'a> {
-    names: rpds::RedBlackTreeMap<&'a str, usize>,
-    named_scopes: rpds::RedBlackTreeMap<Guid, rpds::RedBlackTreeMap<&'a str, usize>>,
+    names_stack: List<RedBlackTreeMap<&'a str, usize>>,
+    named_scopes: RedBlackTreeMap<Guid, RedBlackTreeMap<&'a str, usize>>,
     depth: usize,
+}
+
+impl<'a> Default for Scope<'a> {
+    fn default() -> Self {
+        Scope {
+            names_stack: List::new().push_front(RedBlackTreeMap::default()),
+            named_scopes: RedBlackTreeMap::default(),
+            depth: 0,
+        }
+    }
 }
 
 impl<'a> Scope<'a> {
     pub fn insert_name(&self, key: &'a str) -> Self {
+        let names = self.names_stack.first().expect("Scopes not empty").insert(key, self.depth);
+        let names_stack = self.names_stack.drop_first().expect("Scopes not empty").push_front(names);
+
         Scope {
-            names: self.names.insert(key, self.depth),
+            names_stack,
             named_scopes: self.named_scopes.clone(),
             depth: self.depth + 1,
         }
@@ -22,15 +35,25 @@ impl<'a> Scope<'a> {
 
     pub fn insert_jump(&self, guid: Guid) -> Self {
         Scope {
-            names: self.names.clone(),
-            named_scopes: self.named_scopes.insert(guid, self.names.clone()),
+            names_stack: self.names_stack.clone(),
+            named_scopes: self.named_scopes.insert(guid, self.names_stack.first().expect("Scopes not empty").clone()),
             depth: self.depth,
         }
     }
     
     pub fn jump(&self, guid: Guid) -> Self {
+        let names_stack = self.names_stack.push_front(self.named_scopes[&guid].clone());
+
         Scope {
-            names: self.named_scopes[&guid].clone(),
+            names_stack,
+            named_scopes: self.named_scopes.clone(),
+            depth: self.depth,
+        }
+    }
+
+    pub fn unjump(&self) -> Self {
+        Scope {
+            names_stack: self.names_stack.drop_first().expect("Scopes not empty"),
             named_scopes: self.named_scopes.clone(),
             depth: self.depth,
         }
@@ -70,7 +93,7 @@ impl TcEnv {
                 SourceExpr::Variable(name) => {
                     if name == "_" {
                         PartialExpr::Free
-                    } else if let Some(depth) = scopes[i].names.get(name.as_str()) {
+                    } else if let Some(depth) = scopes[i].names_stack.first().expect("Scopes not empty").get(name.as_str()) {
                         PartialExpr::DeBruijnIndex(scopes[i].depth - depth - 1)
                     } else {
                         self.errors.push(TypeError::UnknownName(*span));
@@ -110,9 +133,9 @@ impl TcEnv {
                     PartialExpr::Shift(UnionIndex(v.index() + start), 0)
                 }
                 SourceExpr::ScopeExit(v) => {
-                    //TODO make names a stack?
-                    //TODO test for multiple depths of syntax: use custom syntax in definition of new syntax
-                    todo!()
+                    //TODO test for multiple depths of syntax: use custom syntax in definition of new syntax  
+                    scopes[v.index()] = scopes[i].unjump();
+                    PartialExpr::Shift(UnionIndex(v.index() + start), 0)
                 }
             };
             scopes.pop();
