58b6bf3c3e5f3b8b4deab190471613266e7d88e4
jonathan <jonathantbrouwer@gmail.com>
2024-08-04 15:55:26 +0200

    Add leaking serde impl

--- prism-parser-bootstrap/src/main.rs
+++ prism-parser-bootstrap/src/main.rs
@@ -2,13 +2,56 @@
 
 use prism_parser::error::aggregate_error::ParseResultExt;
 use prism_parser::error::set_error::SetError;
-use prism_parser::parse_grammar;
-use std::fs::File;
+use prism_parser::grammar::from_action_result::parse_grammarfile;
+use prism_parser::grammar::GrammarFile;
+use prism_parser::rule_action::action_result::ActionResult;
+use prism_parser::rule_action::from_action_result::parse_rule_action;
+use prism_parser::rule_action::RuleAction;
+use prism_parser::{parse_grammar, run_parser_rule_here, META_GRAMMAR};
+use std::fs::{read, File};
 
 fn main() {
+    // normal();
+    // part1();
+    part2();
+}
+
+fn normal() {
     let input = include_str!("../resources/meta.grammar");
     let grammar2 = parse_grammar::<SetError>(input).unwrap_or_eprint();
 
+    // let grammar: &'static GrammarFile = &META_GRAMMAR;
+    // assert_eq!(grammar, &grammar2); // Safety check
+
+    let mut file = File::create("prism-parser/resources/bootstrap.json").unwrap();
+    serde_json::to_writer_pretty(&mut file, &grammar2).unwrap();
+    let mut file = File::create("prism-parser/resources/bootstrap.bincode").unwrap();
+    bincode::serialize_into(&mut file, &grammar2).unwrap();
+}
+
+fn part1() {
+    let input = include_str!("../resources/meta.grammar");
+
+    run_parser_rule_here!(result = &META_GRAMMAR, "toplevel", SetError, input);
+    let result = result.unwrap_or_eprint();
+
+    let mut file = File::create("prism-parser-bootstrap/resources/temp.bincode").unwrap();
+    bincode::serialize_into(&mut file, &result).unwrap();
+}
+
+fn part2() {
+    let input = include_str!("../resources/meta.grammar");
+
+    // Leak because ownership was being annoying
+    let temp: &'static [u8] = Box::leak(
+        read("prism-parser-bootstrap/resources/temp.bincode")
+            .unwrap()
+            .into_boxed_slice(),
+    );
+    let result: ActionResult<'_, 'static> = bincode::deserialize(temp).unwrap();
+
+    let grammar2: GrammarFile<'_, RuleAction<'_, '_>> =
+        parse_grammarfile(&result, input, parse_rule_action).unwrap();
     let mut file = File::create("prism-parser/resources/bootstrap.json").unwrap();
     serde_json::to_writer_pretty(&mut file, &grammar2).unwrap();
     let mut file = File::create("prism-parser/resources/bootstrap.bincode").unwrap();
--- prism-parser/src/rule_action/action_result.rs
+++ prism-parser/src/rule_action/action_result.rs
@@ -1,18 +1,73 @@
 use crate::core::adaptive::RuleId;
 use itertools::Itertools;
-
+#[cfg(feature = "serde_leaking_action_result")]
+use serde::{Deserialize, Serialize};
 use crate::core::span::Span;
 use crate::grammar::escaped_string::EscapedString;
 use crate::parser::var_map::VarMap;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+#[cfg_attr(feature = "serde_leaking_action_result", derive(Serialize, Deserialize))]
 pub enum ActionResult<'arn, 'grm> {
     Value(Span),
     Literal(EscapedString<'grm>),
-    Construct(Span, &'grm str, &'arn [ActionResult<'arn, 'grm>]),
+    Construct(Span, &'grm str, #[cfg_attr(feature = "serde_leaking_action_result", serde(with="leak_slice"))] &'arn [ActionResult<'arn, 'grm>]),
     Guid(usize),
     RuleId(RuleId),
+    #[cfg_attr(feature = "serde_leaking_action_result", serde(skip))]
     WithEnv(VarMap<'arn, 'grm>, &'arn ActionResult<'arn, 'grm>),
+}
+
+#[cfg(feature = "serde_leaking_action_result")]
+pub mod leak_slice {
+    use std::fmt;
+    use std::fmt::Formatter;
+    use std::marker::PhantomData;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use serde::de::{SeqAccess, Visitor};
+    use serde::ser::SerializeSeq;
+
+    pub fn serialize<S: Serializer, T: Serialize>(xs: &[T], s: S) -> Result<S::Ok, S::Error> {
+        let mut seq = s.serialize_seq(Some(xs.len()))?;
+        for x in xs {
+            seq.serialize_element(x)?;
+        }
+        seq.end()
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>, T: Deserialize<'de>>(deserializer: D) -> Result<&'de [T], D::Error> {
+        struct VecVisitor<T> {
+            marker: PhantomData<T>,
+        }
+
+        impl<'de, T> Visitor<'de> for VecVisitor<T>
+        where
+            T: Deserialize<'de>,
+        {
+            type Value = Vec<T>;
+
+            fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
+                formatter.write_str("a sequence")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let mut values = Vec::<T>::with_capacity(seq.size_hint().unwrap_or(0));
+                while let Some(value) = seq.next_element()? {
+                    values.push(value);
+                }
+                Ok(values)
+            }
+        }
+
+        let visitor = VecVisitor {
+            marker: PhantomData,
+        };
+        let vec: Vec<T> = deserializer.deserialize_seq(visitor)?;
+        Ok(vec.leak())
+    }
 }
 
 impl<'arn, 'grm> ActionResult<'arn, 'grm> {
