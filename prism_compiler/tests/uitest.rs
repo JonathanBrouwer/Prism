a6716f6f02d897f6609062e4b905f694e8deeea5
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-01-04 14:14:43 +0100

    Add stderrs

--- prism_compiler/tests/uitest.rs
+++ prism_compiler/tests/uitest.rs
@@ -32,46 +32,24 @@ fn run_uitest(file_path: &Path, args: &UitestArguments) -> Result<(), Failed> {
     let mut env = PrismDb::default();
 
     let input = env.load_file(file_path.into());
+    let (input, _) = env.parse_prism_file(input);
+    let typ = env.type_check(input);
 
-    let input = env.input.inner().get_str(input).to_string();
+    // Compare stderr
+    let mut stderr = String::new();
+    for diag in mem::take(&mut env.diags) {
+        writeln!(
+            &mut stderr,
+            "{}\n",
+            diag.render(&RenderConfig::uitest(), &env.input.inner())
+        )
+        .unwrap();
+    }
+    compare_output(file_path, stderr.as_bytes(), "stderr", args)?;
+    if !stderr.is_empty() {
+        return Ok(());
+    }
 
-    let (input_str, rest) = input.split_once("### Eval\n").unwrap();
-    let (eval, expected_typ_str) = rest.split_once("### Type\n").unwrap();
-
-    std::fs::write(file_path, input_str).unwrap();
-
-    let results_dir = file_path.parent().unwrap().join("_results");
-    std::fs::create_dir_all(&results_dir).unwrap();
-    let eval_path = results_dir
-        .join(file_path.file_name().unwrap())
-        .with_added_extension("eval");
-    std::fs::write(eval_path, eval).unwrap();
-
-    let type_path = results_dir
-        .join(file_path.file_name().unwrap())
-        .with_added_extension("type");
-    std::fs::write(type_path, expected_typ_str).unwrap();
-
-    Ok(())
-
-    // let (input, _) = env.parse_prism_file(input);
-    // let typ = env.type_check(input);
-
-    // // Compare stderr
-    // let mut stderr = String::new();
-    // for diag in mem::take(&mut env.diags) {
-    //     writeln!(
-    //         &mut stderr,
-    //         "{}\n",
-    //         diag.render(&RenderConfig::uitest(), &env.input.inner())
-    //     )
-    //     .unwrap();
-    // }
-    // compare_output(file_path, stderr.as_bytes(), "stderr", args)?;
-    // if !stderr.is_empty() {
-    //     return Ok(());
-    // }
-    //
     // compare_term(file_path, &mut env, input, "parsed", args)?;
     // compare_term(file_path, &mut env, typ, "type", args)?;
     //
@@ -79,8 +57,8 @@ fn run_uitest(file_path: &Path, args: &UitestArguments) -> Result<(), Failed> {
     //
     // let eval = env.beta_reduce(input, &DbEnv::default());
     // compare_term(file_path, &mut env, eval, "eval", args)?;
-    //
-    // Ok(())
+
+    Ok(())
 }
 
 fn compare_term(
@@ -100,10 +78,9 @@ fn compare_output(
     output_ext: &'static str,
     args: &UitestArguments,
 ) -> Result<(), String> {
-    let output_path = file_path
-        .parent()
-        .unwrap()
-        .join("_results")
+    let results_dir = file_path.parent().unwrap().join("_results");
+    std::fs::create_dir_all(&results_dir).unwrap();
+    let output_path = results_dir
         .join(file_path.file_name().unwrap())
         .with_added_extension(output_ext);
     if args.bless {
