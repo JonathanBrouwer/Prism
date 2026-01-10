73d3226ac7d9c7e84c01c7a46a6d1f9dbb6d226e
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-01-10 17:20:17 +0100

    Move stuff

---
+++ prism_lsp/src/lib.rs
@@ -0,0 +1,61 @@
+mod language_server;
+
+use prism_compiler::lang::PrismDb;
+use prism_input::input_table::{InputTableIndex, InputTableInner};
+use prism_input::span::Span;
+use prism_parser::core::tokens::{TokenType, Tokens};
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use std::mem::take;
+use std::ops::DerefMut;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use tower_lsp_server::jsonrpc::Result;
+use tower_lsp_server::ls_types::*;
+use tower_lsp_server::{Client, LanguageServer, LspService, Server};
+
+pub struct LspBackend {
+    client: Client,
+    inner: RwLock<LspBackendInner>,
+}
+
+impl LspBackend {
+    pub fn new(client: Client) -> Self {
+        Self {
+            client,
+            inner: Default::default(),
+        }
+    }
+}
+
+#[derive(Default)]
+struct LspBackendInner {
+    db: PrismDb,
+    documents: HashMap<Uri, OpenDocument>,
+    document_parses: HashMap<InputTableIndex, Arc<Tokens>>,
+}
+
+#[derive(Copy, Clone)]
+enum DocumentType {
+    Prism,
+    PrismGrammar,
+}
+
+impl Display for DocumentType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                DocumentType::Prism => "Prism",
+                DocumentType::PrismGrammar => "Prism Grammar",
+            }
+        )
+    }
+}
+
+struct OpenDocument {
+    index: InputTableIndex,
+    document_type: DocumentType,
+}
