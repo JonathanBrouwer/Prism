f5ed41fdd6267153a9cd4ce002c61cb1478151f4
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-02-20 17:25:14 +0100

    Remove old parser

--- prism_parser/src/grammar/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod annotated_rule_expr;
-pub mod charclass;
-pub mod grammar_file;
-pub mod map_actions;
-pub mod rule;
-pub mod rule_action;
-pub mod rule_annotation;
-pub mod rule_block;
-pub mod rule_expr;
--- prism_parser/src/parsable/guid.rs
+++
@@ -1,2 +0,0 @@
-#[derive(Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
-pub struct Guid(pub usize);
